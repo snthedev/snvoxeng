d66ec08e7c2a24c1fa23ae58b50f6a2378d1cc81
snthedev <snthedev@gmail.com>
2026-08-05 11:14:16 +0300

    improved dumb_vector

--- snvoxeng/snvoxeng/utils/dumb_vector.hpp
+++ snvoxeng/snvoxeng/utils/dumb_vector.hpp
@@ -8,14 +8,32 @@ namespace sn::voxeng
     template<typename T>
     class dumb_vector
     {
-        alignas(alignof(T)) std::byte* m_pBuffer = nullptr;
-        size_t m_capacity = 0;
-        size_t m_size = 0;
+        std::byte* m_pBuffer{ nullptr };
+        size_t m_capacity{ 0 };
+        size_t m_size{ 0 };
 
-        void alloc() { m_pBuffer = new std::byte[sizeof(T) * m_capacity]; }
-        void free() noexcept { clear(); delete[] m_pBuffer; }
+        void alloc()
+        {
+            if (m_capacity == 0) [[unlikely]] return;
+            m_pBuffer = static_cast<std::byte*>(::operator new[](
+                sizeof(T) * m_capacity,
+                std::align_val_t{ alignof(T) }
+                ));
+        }
+        void free() noexcept
+        {
+            if (!m_pBuffer) [[unlikely]] return;
+            clear();
+            ::operator delete[](
+                m_pBuffer,
+                std::align_val_t{ alignof(T) }
+                );
+            m_pBuffer = nullptr;
+            m_capacity = 0;
+        }
 
     public:
+        // call reserve() manually
         dumb_vector() noexcept : m_capacity(0) {}
         explicit dumb_vector(size_t capacity) : m_capacity(capacity) { alloc(); }
         ~dumb_vector() noexcept { free(); }
@@ -33,7 +51,6 @@ namespace sn::voxeng
             if (this != &other)
             {
                 free();
-
                 m_pBuffer = std::exchange(other.m_pBuffer, nullptr);
                 m_capacity = std::exchange(other.m_capacity, 0);
                 m_size = std::exchange(other.m_size, 0);
@@ -52,44 +69,75 @@ namespace sn::voxeng
         template<typename... Args>
         T& emplace_back(Args&&... args)
         {
-            snassert(m_size < m_capacity, "Size cannot be greater than capasity. It's dumb vector", "");
+            snassert(m_size < m_capacity, "Out of dumb_vector capacity", "dumb_vector::size() can not be greater than dumb_vector::capacity()");
 
-            T* ptr = reinterpret_cast<T*>(m_pBuffer + m_size * sizeof(T));
-            ::new (static_cast<void*>(ptr)) T(std::forward<Args>(args)...);
-            m_size++;
-            return *ptr;
+            ::new (static_cast<void*>(begin() + m_size++)) T(std::forward<Args>(args)...);
+            return back();
         }
 
         template<typename Builder, typename... Args>
         T& emplace_builder(Builder&& builder, Args&&... args)
         {
-            snassert(m_size < m_capacity, "Size cannot be greater than capasity. It's dumb vector", "");
+            snassert(m_size < m_capacity, "Out of dumb_vector capacity", "dumb_vector::size() can not be greater than dumb_vector::capacity()");
 
-            T* ptr = reinterpret_cast<T*>(m_pBuffer + m_size * sizeof(T));
-            ::new (static_cast<void*>(ptr)) T(builder.build(std::forward<Args>(args)...));
-            m_size++;
-            return *ptr;
+            ::new (static_cast<void*>(begin() + m_size++)) T(builder.build(std::forward<Args>(args)...));
+            return back();
         }
 
         void clear() noexcept
         {
-            for (size_t i = 0; i < m_size; ++i)
-                reinterpret_cast<T*>(m_pBuffer + i * sizeof(T))->~T();
+            for (T* ptr = begin(); ptr != end(); ++ptr) ptr->~T();
             m_size = 0;
         }
 
-        const T& operator[](size_t idx) const { return *reinterpret_cast<T*>(m_pBuffer + idx * sizeof(T)); }
-        T& operator[](size_t idx) { return *reinterpret_cast<T*>(m_pBuffer + idx * sizeof(T)); }
+        const T& operator[](size_t idx) const
+        {
+            snassert(idx < m_size, "Out of dumb_vector size", "idx must be less than dumb_vector::size()");
+            return *(begin() + idx);
+        }
+        T& operator[](size_t idx)
+        {
+            snassert(idx < m_size, "Out of dumb_vector size", "idx must be less than dumb_vector::size()");
+            return *(begin() + idx);
+        }
 
         operator std::span<const T>() const noexcept
         {
-            return std::span<const T>{ reinterpret_cast<const T*>(m_pBuffer), m_size };
+            return { begin(), m_size };
         }
         operator std::span<T>() noexcept
         {
-            return std::span<T>{ reinterpret_cast<T*>(m_pBuffer), m_size };
+            return { begin(), m_size };
         }
 
+        T* begin() noexcept { return reinterpret_cast<T*>(m_pBuffer); }
+        T* end() noexcept { return begin() + m_size; }
+        const T* begin() const noexcept { return reinterpret_cast<const T*>(m_pBuffer); }
+        const T* end() const noexcept { return begin() + m_size; }
+
+        T& front() noexcept
+        {
+            snassert(!empty(), "Out of dumb_vector", "front() called on empty dumb_vector");
+            return operator[](0);
+        }
+        T& back() noexcept
+        {
+            snassert(!empty(), "Out of dumb_vector", "back() called on empty dumb_vector");
+            return operator[](m_size - 1u);
+        }
+        const T& front() const noexcept
+        {
+            snassert(!empty(), "Out of dumb_vector", "front() called on empty dumb_vector");
+            return operator[](0);
+        }
+        const T& back() const noexcept
+        {
+            snassert(!empty(), "Out of dumb_vector", "back() called on empty dumb_vector");
+            return operator[](m_size - 1u);
+        }
+
+        size_t capacity() const noexcept { return m_capacity; }
         size_t size() const noexcept { return m_size; }
+        bool empty() const noexcept { return m_size == 0; }
     };
 }
